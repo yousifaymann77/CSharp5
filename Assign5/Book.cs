168bd31bad6bb3ed8c0f73e85a586b18705f6407
yousif <yshehab693@gmail.com>
2026-09-06 11:49:23 +0300

    Question 1

---
+++ Assign5/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign5
+{
+    internal class Book
+    {
+        private string password = "secret";
+    }
+}
--- Assign5/Program.cs
+++ Assign5/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Add a private string password = "secret"; field to a Book class. Try to print it from Main(outside the class). What happens, and why?
+            // it will give an error because the password field is private and cannot be accessed from outside the class.
+            Book b = new Book();
+            Console.WriteLine(b.password); 
+            #endregion
         }
     }
 }
