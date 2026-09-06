751acb38ae6978ed39b7617712fc66271b3d555a
yousif <yshehab693@gmail.com>
2026-09-06 11:52:04 +0300

    Question 2

--- Assign5/Book.cs
+++ Assign5/Book.cs
@@ -7,5 +7,6 @@ namespace Assign5
     internal class Book
     {
         private string password = "secret";
+        internal int copiesInStock = 5;
     }
 }
--- Assign5/Program.cs
+++ Assign5/Program.cs
@@ -7,8 +7,14 @@
             #region Add a private string password = "secret"; field to a Book class. Try to print it from Main(outside the class). What happens, and why?
             // it will give an error because the password field is private and cannot be accessed from outside the class.
             Book b = new Book();
-            Console.WriteLine(b.password); 
+            //Console.WriteLine(b.password);
             #endregion
+
+            #region Add an internal int copiesInStock = 5; field to Book. Print it from Main. Does it compile?Why?
+            // it will compile because the copiesInStock field is internal and can be accessed from within the same class.
+            Console.WriteLine(b.copiesInStock); 
+            #endregion
+
         }
     }
 }
